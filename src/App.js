db97ab205d2ea3c9de1972248ee38bf6b32eb2a2
Mitch Fox <mjfox99@gmail.com>
2022-05-17 01:35:33 +1000

    design

--- src/App.js
+++ src/App.js
@@ -1,37 +1,63 @@
+import React from 'react';
 import './App.css';
 import { Column, Row } from "simple-flexbox";
 
-// Simple App 
+// Chart Components
+import Chart1 from './Components/Chart1';
 
+
+
+// Simple Responsive WebApp showcasing various graphing components
 function App() {
   return (
     <div className="App">
       <div className="content">
+        <div className="container">
+          <p className="text">Simple Responsive WebApp showcasing various graphing components</p>
+        </div>
         <Column style={{ width: "100%" }}>
           <Row
             wrap
             flexGrow={1}
             vertical="center"
             horizontal="spaced"
-            style={{ backgroundColor: "grey" }}
+          //style={{ backgroundColor: "grey" }}
           >
 
             <Column
               flexGrow={1}
-            style={{ backgroundColor: "blue", color: "white", minWidth: "300px" }}
+              style={{ color: "white" }}
             >
-              <span> Heatmap </span>
-              <span> Graph 1 </span>
+              <div className="graphContainer">
+                <Chart1 />
+              </div>
+
+              <div className="graphContainer">
+                Graph 2
+              </div>
+
+              <div className="graphContainer">
+                Graph 3
+              </div>
+
             </Column>
 
 
             <Column
               flexGrow={1}
-            style={{ backgroundColor: "red", color: "white", minWidth: "300px" }}
+              style={{ color: "white" }}
             >
-              <span> Content 3 </span>
-              <span> Content 4 </span>
-              <span> Content 5 </span>
+           <div className="graphContainer">
+                Graph 4
+              </div>
+
+              <div className="graphContainer">
+                Graph 5
+              </div>
+
+              <div className="graphContainer">
+                Graph 6
+              </div>
             </Column>
           </Row>
         </Column>
